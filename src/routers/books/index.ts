41f97d0055a4d74b00ed595a636614a0675aeb74
v0sska <speedatom80k@gmail.com>
2024-06-26 11:14:32 +0300

    Created a router for books

---
+++ src/routers/books/index.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const bookRouter = Router();
+
+bookRouter.post('', );
+bookRouter.get('', );
+bookRouter.get('/:id', );
+bookRouter.put('/:id', );
+bookRouter.delete('/:id', );
+
+export default bookRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
+import  router  from './routers';
 
 const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use(express.json());
+
+app.use('/', router);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
